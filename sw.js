00046592de59227814097754b370c2ec7cac3278
Sangeeth995 <52905761+Sangeeth995@users.noreply.github.com>
2026-01-01 18:26:58 +0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'lifebank-v1';
+const FILES = [
+  '.',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', evt => {
+  evt.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', evt => {
+  evt.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(resp => resp || fetch(evt.request))
+  );
+});
